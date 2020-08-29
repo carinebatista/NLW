6e25af63d558837f73ee83c1e0405fa17fdc5bc8
Carine Batista <carinedcb@hotmail.com>
2020-08-29 16:38:10 -0300

    Add data from backend

--- src/server.js
+++ src/server.js
@@ -1,3 +1,30 @@
+
+const proffys= [ 
+    {name:"Carine Batista",
+    avatar: "https://avatars2.githubusercontent.com/u/18583716?s=460&v=4",
+    whatsapp: "992489970",
+    bio:'Entusiasta da área de Veterinária, com uma paixão imensa por animais <br> <Br> Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
+    subject:'Veterinária', 
+    cost:'20',
+    weekday: [0], 
+    time_from:[720],
+    time_to: [1220] 
+    },
+
+    { name:"Daniele Evangelista",
+    avatar: "https://avatars2.githubusercontent.com/u/18583716?s=460&v=4",
+    whatsapp: "992489970",
+    bio:'Entusiasta da área de Química, com uma paixão imensa por animais <br> <Br> Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
+    subject:'Química', 
+    cost:'20',
+    weekday: [1], 
+    time_from:[720],
+    time_to: [1220] 
+    }
+] 
+
+
+
 const express = require('express')
 const server = express()
 
