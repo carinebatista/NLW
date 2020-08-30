598a9a5bbe795928b6b7915eb350b4e8813aa136
Carine Batista <carinedcb@hotmail.com>
2020-08-30 00:49:33 -0300

    Add server static files

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const proffys= [ 
     {name:"Carine Batista",
     avatar: "https://avatars2.githubusercontent.com/u/18583716?s=460&v=4",
-    whatsapp: "992489970",
+    whatsapp: "19992489970",
     bio:'Entusiasta da área de Veterinária, com uma paixão imensa por animais <br> <Br> Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
     subject:'Veterinária', 
     cost:'20',
@@ -11,8 +11,8 @@ const proffys= [
     time_to: [1220] 
     },
 
-    { name:"Daniele Evangelista",
-    avatar: "https://avatars2.githubusercontent.com/u/18583716?s=460&v=4",
+    { name:"Mayk Brito",
+    avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=44",
     whatsapp: "992489970",
     bio:'Entusiasta da área de Química, com uma paixão imensa por animais <br> <Br> Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
     subject:'Química', 
@@ -27,8 +27,13 @@ function pageLanding(req,res){
     return res.render("index.html")
 }
 function pageStudy(req, res){
-    return res.render("study.html")
+    return res.render("study.html", { proffys })
 }
+
+
+https://avatars2.githubusercontent.com/u/18583716?s=460&v=4
+
+
 
 function pageGiveClasses(req,res){
     return res.render("give-classes.html")
@@ -53,7 +58,5 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
-
-
 .listen(5000)
 
