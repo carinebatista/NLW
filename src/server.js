19e765feda0fbccc88819a24fa85046a0d84dbd0
Carine Batista <carinedcb@hotmail.com>
2020-09-02 09:57:28 -0300

    Add subject Get

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const proffys= [
     {name:"Carine Batista",
     avatar: "https://avatars2.githubusercontent.com/u/18583716?s=460&v=4",
     whatsapp: "19992489970",
-    bio:'Entusiasta da área de Veterinária, com uma paixão imensa por animais <br> <Br> Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
+    bio:'Entusiasta da área de Veterinária, com uma paixão imensa por animais. Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
     subject:'Veterinária', 
     cost:'20',
     weekday: [0], 
@@ -14,7 +14,7 @@ const proffys= [
     { name:"Mayk Brito",
     avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=44",
     whatsapp: "992489970",
-    bio:'Entusiasta da área de Química, com uma paixão imensa por animais <br> <Br> Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
+    bio:'Entusiasta da área de Química, com uma paixão imensa por animais. Apaixonada por cuidar e ensinar, e por mudar a vida de pessoas atraves do tratamento paliativo e Ozonioterápico. Mais de 200. 000 pessoas já passaram por essa imersão de cuidado e amor',
     subject:'Química', 
     cost:'20',
     weekday: [1], 
@@ -46,6 +46,13 @@ const weekdays = [
      "Sábado",
 ]
 
+// Funcionalidades
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1
+    return  subjects[position]
+}
+
+
 function pageLanding(req,res){
     return res.render("index.html")
 }
@@ -55,7 +62,21 @@ function pageStudy(req, res){
 }
 
 function pageGiveClasses(req,res){
-    return res.render("give-classes.html")
+    const data=  req.query
+     
+    // Se tiver dados (data)
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty){
+
+        data.subject = getSubject(data.subject)
+        // Adicionar dados a lista de proffys
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    // senão, mostrar a pagina
+    return res.render("give-classes.html",{ subjects, weekdays })
 }
 
 
