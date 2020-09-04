91d7daefdb1f9b15fa5ac02be42fabfd74e9c1fa
Carine Batista <carinedcb@hotmail.com>
2020-09-03 21:57:53 -0300

    Add SQL tables- classes and proffy

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,22 @@ const Database = require('sqlite-async')
 Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db){
-    console.log('cheguei aqui')
-    console.log(db)
+     // Criar as tabelas do banco de dados
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS proffys (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            whatsapp TEXT,
+            bio TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS classes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            cost TEXT,
+            proffy_id INTEGER
+        );
+
+    `)
 }
