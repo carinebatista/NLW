816c6e871ad86e7b28fa5bb8406be1bf19affb60
Carine Batista <carinedcb@hotmail.com>
2020-09-07 22:54:05 -0300

    Add proffy to table by test

--- src/database/test.js
+++ src/database/test.js
@@ -1,1 +1,13 @@
-const db = require('./db')
+const Database = require('./db')
+
+Database.then((db) => {
+    // Inserir dados
+    proffy ={
+        name: 'Carine Batista',
+        avatar: 'https://avatars2.githubusercontent.com/u/18583716?s=460&v=4',
+        whatsapp: '19992489970', 
+        bio:'Instrutora de Educação Física'
+    }
+    // Consultar os dados inseriddos
+
+})
