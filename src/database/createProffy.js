843342b7c517c1981c56ccb61c26909e4c486631
Carine Batista <carinedcb@hotmail.com>
2020-09-10 23:19:30 -0300

    Add values into class

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -13,5 +13,24 @@ module.exports = async function(db, {proffyValue, classValue, classScheduleValue
                 ${proffyValue.bio}
             );
     `)
-   
+   const proffy_id = insertedProffy.lastID
+    //inserir dados na tabela classes
+    const insertedClass = await db.run(`
+                INSERT INTO classes(
+                    subject,
+                    cost, 
+                    proffy_id
+                ) VALUES(
+                    ${classValue.subject},
+                    ${classValue.cost},
+                    ${proffy_id}
+                );
+    `)
+
+    const class_id = insertedProffy.lastID
+
+    // Inserir dados na tabela class_schedule
+
+    
+
 }
