6bb7f88c2cbf3895b31c6dbf955dba8ce60d6f91
Carine Batista <carinedcb@hotmail.com>
2020-09-10 23:15:57 -0300

    Add values into table

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -1,10 +1,17 @@
 module.exports = async function(db, {proffyValue, classValue, classScheduleValue}){
     // inserir dados na table de teachers
-
-        // ` é chamada de template Literals em JavaScipt
     const insertedProffy = await db.run(`
-    
-    
+            INSERT INTO proffys (
+                name, 
+                avatar, 
+                whatsapp,
+                bio
+            )  VALUES(
+                ${proffyValue.name},
+                ${proffyValue.avatar},
+                ${proffyValue.whatsapp},
+                ${proffyValue.bio}
+            );
     `)
    
 }
